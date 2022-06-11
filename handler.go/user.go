329f99f34d401a5f20523fc62775466ab7c76d66
rizky ramadhan <rizkyramadhanajjkh@gmail.com>
2022-06-12 03:36:54 +0700

    init project

---
+++ handler.go/user.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"campaignfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap pinput dari user
+	// map input dari user ke struct RegisterUserInput
+	// struct di atas kita parsing sebagai parameter service
+	var input user.RegisterUserInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+	c.JSON(http.StatusOK, user)
+}
